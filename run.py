8ea1a7b61937214e273fe3ec42921a4a8298d7bb
Yuriy Jurayev <jurayevy@ryanair.com>
2019-12-29 20:35:47 +0100

    refactor: setting main function

--- run.py
+++ run.py
@@ -2,25 +2,31 @@ import random
 from src import helpers, model, preprocess
 from tests import unit_tests
 
-# Image data directories
-IMAGE_DIR_TEST = "traffic_light_images/test/"
 
-# Using the load_dataset function in helpers.py
-# Load tests data
-TEST_IMAGE_LIST = helpers.load_dataset(IMAGE_DIR_TEST)
+def main():
+    # Image data directories
+    IMAGE_DIR_TEST = "traffic_light_images/test/"
 
-# Standardize the tests data
-STANDARDIZED_TEST_LIST = preprocess.standardize(TEST_IMAGE_LIST)
+    # Using the load_dataset function in helpers.py
+    # Load tests data
+    TEST_IMAGE_LIST = helpers.load_dataset(IMAGE_DIR_TEST)
 
-# Shuffle the standardized tests data
-random.shuffle(STANDARDIZED_TEST_LIST)
+    # Standardize the tests data
+    STANDARDIZED_TEST_LIST = preprocess.standardize(TEST_IMAGE_LIST)
 
-# Find all misclassified images in a given tests set
-MISCLASSIFIED = model.get_misclassified_images(STANDARDIZED_TEST_LIST)
+    # Shuffle the standardized tests data
+    random.shuffle(STANDARDIZED_TEST_LIST)
 
-model.calculate_accuracy(STANDARDIZED_TEST_LIST, MISCLASSIFIED)
+    # Find all misclassified images in a given tests set
+    MISCLASSIFIED = model.get_misclassified_images(STANDARDIZED_TEST_LIST)
 
-# TESTS
-tests = unit_tests.Tests()
-tests.test_one_hot(preprocess.one_hot_encode)
-tests.test_red_as_green(MISCLASSIFIED)
+    model.calculate_accuracy(STANDARDIZED_TEST_LIST, MISCLASSIFIED)
+
+    # TESTS
+    tests = unit_tests.Tests()
+    tests.test_one_hot(preprocess.one_hot_encode)
+    tests.test_red_as_green(MISCLASSIFIED)
+
+
+if __name__ == '__main__':
+    main()
