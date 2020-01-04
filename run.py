cb117ee9d971284817fab703456e04d93d97224b
jurayev <yuriy.jurayev.d@gmail.com>
2020-01-04 22:04:41 +0100

    test: Extract all test into separate method

--- run.py
+++ run.py
@@ -3,6 +3,15 @@ from src import helpers, model, preprocess
 from tests import unit_tests
 
 
+def run_unittest(misclassified_images):
+    print("--------------------------RUNNING UNIT TESTS--------------------------------")
+    tests = unit_tests.Tests()
+    tests.test_one_hot_encode_red()
+    tests.test_one_hot_encode_yellow()
+    tests.test_one_hot_encode_green()
+    tests.test_red_as_green(misclassified_images)
+
+
 def main():
     # Image data directories
     IMAGE_DIR_TEST = "traffic_light_images/test/"
@@ -23,9 +32,7 @@ def main():
     model.calculate_accuracy(STANDARDIZED_TEST_LIST, MISCLASSIFIED)
 
     # TESTS
-    tests = unit_tests.Tests()
-    tests.test_one_hot(preprocess.one_hot_encode)
-    tests.test_red_as_green(MISCLASSIFIED)
+    run_unittest(MISCLASSIFIED)
 
 
 if __name__ == '__main__':
